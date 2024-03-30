3cf98ce37b3f256c03dfd9e97b688ca917161fee
Ruslan Mikhailov <ruslan.dmmc@gmail.com>
2024-03-30 17:20:16 +0400

    add vanilla window format of data

--- time_wise_rat/utils.py
+++ time_wise_rat/utils.py
@@ -9,3 +9,11 @@ def construct_patches(
     array = np.lib.stride_tricks.sliding_window_view(array, num_patches)  # (N, N_p)
     array = np.lib.stride_tricks.sliding_window_view(array, patch_length, axis=0)  # (N, N_p, L_p)
     return array
+
+
+def construct_windows(
+        array: np.ndarray,
+        window_length: int
+) -> np.ndarray:
+    array = np.lib.stride_tricks.sliding_window_view(array, window_length)  # (N, L_w)
+    return array
